8daae617920ce1651e421318dfc4f87f8cd7e174
Kill Toxic <913087364@qq.com>
2020-03-06 14:52:47 +0800

    drawCircleLineRectangle

--- drawCircle/drawCircle.cpp
+++ drawCircle/drawCircle.cpp
@@ -1,20 +1,35 @@
-﻿// drawCircle.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat M1 = cv::imread("D:\\Files\\2.jpg");//读取图片
+	cv::Mat M2;
+	cv::Mat M3;
+	M1.copyTo(M2);
+	M1.copyTo(M3);
+	cv::Point pt;//定义圆心
+	pt.x = 10;
+	pt.y = 10;
+	circle(M1, pt, 5, CV_RGB(255, 0, 0), 1, 8, 0);//画圆
+	cv::Point pt1;//定义线段起点
+	cv::Point pt2;//定义线段终点
+	pt1.x = 10;
+	pt1.y = 10;
+	pt2.x = 20;
+	pt2.y = 20;
+	line(M2, pt1, pt2, CV_RGB(255, 0, 0), 1, 8, 0);//画线段
+	cv::Rect rect;//定义矩形坐标
+	rect.x = 10;
+	rect.y = 10;
+	rect.height=20;//定义矩形高
+	rect.width=60;//定义举行宽
+	rectangle(M3, rect, CV_RGB(255, 0, 0), 1, 8, 0);//画矩形
+	cv::imshow("circle", M1);//显示三张图片
+	cv::imshow("line", M2);
+	cv::imshow("rectangle", M3);
+	waitKey(0);
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
